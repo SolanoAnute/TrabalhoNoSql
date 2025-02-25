39adac22c57e6c25b25079c3e27eb8cb60a8d9b4
SolanoAnute <solanute@gmail.com>
2025-02-25 12:40:20 -0300

    conexao.py

--- futeboldb/conexao.py
+++ futeboldb/conexao.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # Substitua pelos seus dados de conexão
-uri = "mongodb+srv://solanute:123@cluster0.fpr24.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = ""
 
 # Criar função para obter a conexão
 def get_db():
